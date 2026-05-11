f8a7c56288bbe9ad16b475cafe2f87615217d2d7
deniomasive <deniomasive96@gmail.com>
2026-05-11 17:09:52 +0200

    Primeiro commit - backend + frontend configurados

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: {
     proxy: {
       "/api": "http://localhost:8080",
-      "/resolucoes": "http://localhost:8080"
+      "/exames": "http://localhost:8080"
     }
   }
 });
-
